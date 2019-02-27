ca8ccfe5812a03a3caf9e1235956c95935b9e3f3
nukosuke <nukosuke@lavabit.com>
2019-02-27 18:55:15 +0900

    Configure neotree with all-the-icons

--- site-lisp/workspace.el
+++ site-lisp/workspace.el
@@ -20,7 +20,15 @@
 ;; Neotree
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package neotree)
+(use-package neotree
+  :bind
+  ("<f8>" . neotree-toggle)
+
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-show-hidden-files t))
+
+(use-package all-the-icons)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Projectile
