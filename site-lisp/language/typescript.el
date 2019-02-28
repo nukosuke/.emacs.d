b69466b9f1bca06da76190d2886840c6cb1135e3
nukosuke <nukosuke@lavabit.com>
2019-02-28 11:57:06 +0900

    Add tide for TypeScript

--- site-lisp/language/typescript.el
+++ site-lisp/language/typescript.el
@@ -16,9 +16,31 @@
 
 ;;; Code:
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Major mode for TypeScript
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use-package typescript-mode
+  ;; defer load until open any .ts file.
+  :mode
+  "\\.ts\\'"
+
   :custom
   (typescript-indent-level 2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tide:
+;;   TypeScript Interactive Development Environment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package tide
+  :after
+  (typescript-mode company flycheck)
+
+  :hook
+  ((typescript-mode . tide-setup)
+   (typescript-mode . tide-hl-identifier-mode)
+   (before-save     . tide-format-before-save)))
 
 (provide 'typescript)
 
