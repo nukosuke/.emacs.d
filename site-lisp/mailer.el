bc91b38931ecaf1f0466851fdd992774ba22dd0f
nukosuke <nukosuke@lavabit.com>
2019-03-07 23:59:49 +0900

    Fix mail config

--- site-lisp/mailer.el
+++ site-lisp/mailer.el
@@ -1,16 +1,35 @@
 ;;; mailer.el --- RMAIL config -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2019- nukosuke.
+;;
+;; Author:  nukosuke <nukosuke@lavabit.com>
+;; URL:     https://github.com/nukosuke/.emacs.d
+;; License: GPLv3+
+;;
 ;;; Commentary:
+;;
+;;  These files are NOT part of GNU Emacs.
+;;
+;;  * 2019/03/07:
+;;    Create mailer.el
+;;    - Configure IMAP/SMTP for Lavabit
+
 ;;; Code:
 
 (use-package rmail
   :straight nil ;; rmail is built-in Emacs
 
   :custom
+  ;; Receiving: IMAP/SSL
   (rmail-remote-password-required t)
-  (rmail-primary-inbox-list '("imaps://yosuke.tamura%40mixi.co.jp@imap.gmail.com:993"))
+  (rmail-primary-inbox-list '("imaps://nukosuke@lavabit.com:993"))
 
-  :config
-  (setenv "MAILHOST" "lavabit.com"))
+  ;; Sending: SMTP/SSL
+  (user-mail-address     "nukosuke@lavabit.com")
+  (smtpmail-smtp-server  "lavabit.com")
+  (send-mail-function    'smtpmail-send-it)
+  (smtpmail-stream-type  'ssl)
+  (smtpmail-smtp-service 465))
 
 (provide 'mailer)
 
