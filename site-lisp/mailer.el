2a9ee09bc425e0acd2d35c9d8ce821163ef5d568
nukosuke <nukosuke@lavabit.com>
2019-03-07 16:01:19 +0900

    Remove gnus config

--- site-lisp/mailer.el
+++ site-lisp/mailer.el
@@ -1,17 +1,6 @@
-;;; mailer.el --- Gnus config -*- lexical-binding: t; -*-
+;;; mailer.el --- RMAIL config -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
-
-(use-package gnus
-  :straight nil
-
-  :custom
-  (user-full-name "Yosuke Tamura")
-  (user-mail-address "yosuke.tamura@mixi.co.jp")
-  (gnus-select-method '(nnimap "gmail"
-                               (nnimap-address "imap.gmail.com")
-                               (nnimap-server-port 993)
-                               (nnimap-stream ssl))))
 
 (use-package rmail
   :straight nil ;; rmail is built-in Emacs
