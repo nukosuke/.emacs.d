1861ed7b39527e13962edfe3bf84bce9a5581dc2
nukosuke <nukosuke@lavabit.com>
2019-03-27 10:18:36 +0900

    Custom dashboard items

--- site-lisp/interface.el
+++ site-lisp/interface.el
@@ -102,6 +102,9 @@
   :custom
   (dashboard-banner-logo-title (concat "GNU Emacs " emacs-version))
   (dashboard-startup-banner    'logo)
+  (dashboard-items             '((recents  . 5)
+                                 (projects . 5)
+                                 (agenda   . 5)))
 
   :config
   (dashboard-setup-startup-hook))
