869989becb99171dfe9e0e32c0848fb8fb2e414f
nukosuke <nukosuke@lavabit.com>
2019-03-29 16:53:24 +0900

    taihi

--- site-lisp/completion.el
+++ site-lisp/completion.el
@@ -44,6 +44,49 @@
   counsel
 
   :config
+  ;; ivy-rich customize
+  (defun ivy-rich-switch-buffer-icon (candidate)
+    "Get icon from all-the-icons by CANDIDATE type."
+    (with-current-buffer
+        (get-buffer candidate)
+      (let ((icon (all-the-icons-icon-for-mode major-mode)))
+        (if (symbolp icon)
+            (all-the-icons-icon-for-mode 'fundamental-mode)
+          icon))))
+
+  (setq ivy-rich--display-transformers-list
+   '(ivy-switch-buffer
+     (:columns
+      ((ivy-rich-switch-buffer-icon :width 2)
+       (ivy-rich-candidate (:width 30))
+       (ivy-rich-switch-buffer-size (:width 7))
+       (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+       (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+       (ivy-rich-switch-buffer-project (:width 15 :face success))
+       (ivy-rich-switch-buffer-path (:width (lambda (x)
+                                              (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+      :predicate
+      (lambda (cand)
+        (get-buffer cand)))
+     counsel-M-x
+     (:columns
+      ((counsel-M-x-transformer (:width 40))  ; thr original transfomer
+       (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the command
+     counsel-describe-function
+     (:columns
+      ((counsel-describe-function-transformer (:width 40))  ; the original transformer
+       (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the function
+     counsel-describe-variable
+     (:columns
+      ((counsel-describe-variable-transformer (:width 40))  ; the original transformer
+       (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))  ; return the docstring of the variable
+     counsel-recentf
+     (:columns
+      ((ivy-rich-candidate (:width 0.8)) ; return the candidate itself
+       ; return the last modified time of the file
+       (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))))
+
+;  :config
   (ivy-rich-mode 1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
