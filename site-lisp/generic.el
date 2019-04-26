6f2f67c3b5886ed34e8751d01e496eec6e4ec72d
nukosuke <nukosuke@lavabit.com>
2019-04-26 21:29:21 +0900

    Move ddskk config to ime.el

--- site-lisp/generic.el
+++ site-lisp/generic.el
@@ -128,42 +128,6 @@
         ("C-a" . org-seq-home)
         ("C-e" . org-seq-end)))
 
-;; SKK
-;; TODO: input-method.elに移動
-
-(use-package ddskk
-  :commands
-  skk-mode
-
-  :custom
-  (skk-sticky-key  ";")
-  (skk-show-inline 'vertical)
-
-  ;; Usse AZIK input method
-  (skk-use-azik t)
-  (skk-azik-keyboard-type 'us101)
-
-  ;; Use doom-dracula colors
-  ;; https://github.com/hlissner/emacs-doom-themes/blob/master/themes/doom-dracula-theme.el#L38-L67
-  (skk-cursor-hiragana-color "#61bfff")
-  (skk-cursor-katakana-color "#ff79c6")
-
-  :init
-  ;; SKK faces should be set before load for customizing
-  (defface skk-emacs-hiragana-face nil nil)
-  (set-face-attribute 'skk-emacs-hiragana-face nil
-                      :background "#bd93f9"
-                      :foreground "black"
-                      :bold t)
-  (defface skk-emacs-katakana-face nil nil)
-  (set-face-attribute 'skk-emacs-katakana-face nil
-                      :background "#bd93f9"
-                      :foreground "black"
-                      :bold t)
-
-  :bind
-  ("C-c j" . skk-mode))
-
 (provide 'generic)
 
 ;;; generic.el ends here
