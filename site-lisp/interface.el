4a5f98c81f8125ddbb4350d7ee0a54389a6efdea
nukosuke <nukosuke@lavabit.com>
2019-07-29 22:29:51 +0900

    Change modified icon for centaur-tabs
    * Use Fontawesome circle icon
    
    modified:   site-lisp/interface.el

--- site-lisp/interface.el
+++ site-lisp/interface.el
@@ -122,7 +122,7 @@
   (centaur-tabs-set-bar             'left)
   (centaur-tabs-set-icons           t)
   (centaur-tabs-set-modified-marker t)
-  (centaur-tabs-modified-marker     "*")
+  (centaur-tabs-modified-marker     (all-the-icons-faicon "circle" :v-adjust 0.05))
 
   :config
   (centaur-tabs-mode t)
