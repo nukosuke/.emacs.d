7ae15952ed056373f1d5533d4dfad4a6048cd793
nukosuke <nukosuke@lavabit.com>
2019-08-16 18:59:12 +0900

    Disable GUI bars only when display-graphic is non nil
    * scroll-bar-mode, tool-bar-mode menu-bar-mode
    
    modified:   site-lisp/interface.el

--- site-lisp/interface.el
+++ site-lisp/interface.el
@@ -28,9 +28,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Disabled modes
-(tool-bar-mode   0)
-(menu-bar-mode   0)
-(scroll-bar-mode 0)
+(if (display-graphic-p)
+    (progn
+      (scroll-bar-mode 0)
+      (tool-bar-mode   0)
+      (menu-bar-mode   0)))
 
 ;; Enabled modes
 (winner-mode         1)
