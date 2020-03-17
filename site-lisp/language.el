da69cbdbd48791c8bb84934c81f70dca317a9c98
nukosuke <nukosuke@users.noreply.github.com>
2020-03-17 11:29:48 +0900

    Use web-mode for Embeded Elixir template

--- site-lisp/language.el
+++ site-lisp/language.el
@@ -97,8 +97,10 @@
 
 (use-package web-mode
   :mode
-  ("\\.html?\\'" . web-mode)
-  ("\\.erb\\'"   . web-mode)
+  "\\.html?\\'"
+  "\\.erb\\'"
+  "\\.eex\\'"
+
   :custom
   (web-mode-markup-indent-offset 2)
   (web-mode-css-indent-offset    2))
