ccb0adbc524d47c799b6774f516679169a38a570
nukosuke <nukosuke@users.noreply.github.com>
2022-02-22 01:17:52 +0900

    Bump elixir-ls server

--- lsp/elixir/install.sh
+++ lsp/elixir/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.6.5/elixir-ls.zip
+wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.9.0/elixir-ls.zip
 unzip elixir-ls.zip
 rm -f elixir-ls.zip
