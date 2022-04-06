94bc786613babb62346135e7369a464d9c905bc0
nukosuke <nukosuke@users.noreply.github.com>
2022-04-06 14:58:05 +0900

    Fix font settings

--- site-lisp/generic.el
+++ site-lisp/generic.el
@@ -47,29 +47,18 @@
 ;; Font settings
 ;;
 (if (memq window-system '(mac ns))
-    (let* ((font-family "Menlo")
-           (font-size 12)
-           (font-height (* font-size 10))
-           (jp-font-family "ヒラギノ角ゴ ProN"))
-      (set-face-attribute 'default nil :family font-family :height font-height)
-      (let ((name (frame-parameter nil 'font))
-            (jp-font-spec (font-spec :family jp-font-family))
-            (jp-characters '(katakana-jisx0201
-                             cp932-2-byte
-                             japanese-jisx0212
-                             japanese-jisx0213-2
-                             japanese-jisx0213.2004-1))
-            (font-spec (font-spec :family font-family))
-            (characters '((?\u00A0 . ?\u00FF)    ; Latin-1
-                          (?\u0100 . ?\u017F)    ; Latin Extended-A
-                          (?\u0180 . ?\u024F)    ; Latin Extended-B
-                          (?\u0250 . ?\u02AF)    ; IPA Extensions
-                          (?\u0370 . ?\u03FF)))) ; Greek and Coptic
-        (dolist (jp-character jp-characters)
-          (set-fontset-font name jp-character jp-font-spec))
-        (dolist (character characters)
-          (set-fontset-font name character font-spec))
-        (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.2)))))
+    (let* ((size 15)
+           (ascii-font "PlemolJP")
+           (jp-font "PlemolJP")
+           (font-height (* size 10))
+           (font-spec (font-spec :family ascii-font))
+           (jp-font-spec (font-spec :family jp-font)))
+      (set-face-attribute 'default nil :family ascii-font :height font-height)
+      (set-fontset-font nil 'japanese-jisx0212 jp-font-spec)
+      (set-fontset-font nil 'japanese-jisx0213-2 jp-font-spec)
+      (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-font-spec)
+      (set-fontset-font nil '(#x0080 . #x024F) font-spec)
+      (set-fontset-font nil '(#x0370 . #x03FF) font-spec)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Import $PATH from shell
