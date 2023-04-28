45284d045ab2e35069fe7243a8fa1bc41da26085
nukosuke <nukosuke@lavabit.com>
2023-04-28 13:01:38 +0900

    Bump Elixir-LS (#94)
    Co-authored-by: nukosuke <nukosuke@users.noreply.github.com>

--- lsp/elixir/install.sh
+++ lsp/elixir/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.13.0/elixir-ls.zip
+rm -rf *.ez debugger.sh debugger.bat launch.sh language_server.sh language_server.bat
+wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.14.3/elixir-ls.zip
 unzip elixir-ls.zip
 rm -f elixir-ls.zip
