bdf3db99c4cda5941a034bd05bb09e1298246563
nukosuke <nukosuke@lavabit.com>
2023-08-01 13:21:01 +0900

    Bump pkgs (#98)
    * Bump pkgs
    
    ---------
    
    Co-authored-by: nukosuke <nukosuke@users.noreply.github.com>

--- lsp/elixir/install.sh
+++ lsp/elixir/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+version=v0.15.1
 rm -rf *.ez debugger.sh debugger.bat launch.sh language_server.sh language_server.bat
-wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.14.3/elixir-ls.zip
-unzip elixir-ls.zip
-rm -f elixir-ls.zip
+wget https://github.com/elixir-lsp/elixir-ls/releases/download/$version/elixir-ls-$version.zip
+unzip elixir-ls-$version.zip
+rm -f elixir-ls-$version.zip
