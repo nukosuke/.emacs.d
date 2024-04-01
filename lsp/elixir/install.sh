3b0e715a1d0555057055f8cf973809ddf07ced88
nukosuke <39.nuko@gmail.com>
2024-04-01 13:46:54 +0900

    Bump pkgs (#100)
    * Bump pkgs
    
    * Makefile: lsp server installation
    
    * Bump elixir-ls
    
    ---------
    
    Co-authored-by: Yosuke Tamura <17716649+nukosuke@users.noreply.github.com>

--- lsp/elixir/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-version=v0.15.1
-rm -rf *.ez debugger.sh debugger.bat launch.sh language_server.sh language_server.bat
-wget https://github.com/elixir-lsp/elixir-ls/releases/download/$version/elixir-ls-$version.zip
-unzip elixir-ls-$version.zip
-rm -f elixir-ls-$version.zip
